1fb371a835282b76cd5c3096cb97f80b64d0c7f8
= <=>
2016-12-06 11:39:06 -0800

    Missing Element and Largest Continuous Sum Exercises

---
+++ LargestContinuous Sum.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaselineResources
+{
+    class LargestContinuous_Sum
+    {
+        private static void Main(string[] args)
+        {
+            int[] aintArrayToCheck = new int[] { 1, 2, -1, 3, 4, 10, 10, -10, -1 };
+            int[] aintArrayToCheck2 = new int[] { -1, 1 };
+            int intLargestSum = 0;
+            intLargestSum = FindTheLargestContinuousSum(aintArrayToCheck2);
+            Console.WriteLine("The Largest Continuous Sum = " + intLargestSum.ToString());
+            Console.ReadLine();
+        }
+
+
+        // ------------------------------------------------------------
+        // Name: FindTheLargestContinuousSum
+        // Purpose: Loop through both sorted arrays at the same time to
+        //          determine the missing element
+        // ------------------------------------------------------------
+        private static int FindTheLargestContinuousSum(int[] aintArray)
+        {
+            int intLargestSum = 0;
+            int intCurrentSum = 0;
+
+            // Any items to summate?
+            if (aintArray.Length > 0)
+            {
+                // Set our max sum as the first element
+                intLargestSum = aintArray[0];
+                intCurrentSum = aintArray[0];
+                
+                // For every element in the array
+                for (int intIndex = 1; intIndex < aintArray.Length; intIndex += 1)
+                {
+                    // Set the current sum as the higher of the two
+                    // e.g. 10+12/10 || 10-8/-8
+                    intCurrentSum = Math.Max(intCurrentSum + aintArray[intIndex], aintArray[intIndex]);
+                    //Console.WriteLine("CurrentSum: " + intCurrentSum.ToString());
+
+                    // Set the max as the higher between the current sum and the current max
+                    intLargestSum = Math.Max(intCurrentSum, intLargestSum);
+                    //Console.WriteLine("LargestSum: " + intLargestSum.ToString());
+                }
+            }
+
+            return intLargestSum;
+        }
+    }
+}
---
+++ MissingElement.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaselineResources
+{
+    class MissingElement
+    {
+        private static void Main(string[] args)
+        {
+            int[] aintArrayOne = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            int[] aintArrayTwo = new int[] { 1, 2, 3, 4, 5, 6 };
+            int intElement = 0;
+            intElement = FindTheMissingElement_VersionTwo(aintArrayOne, aintArrayTwo);
+            Console.WriteLine("Missing Element: " + intElement.ToString());
+            Console.ReadLine();
+        }
+
+
+        // ------------------------------------------------------------
+        // Name: FindTheMissingElement_VersionOne
+        // Purpose: Loop through both sorted arrays at the same time to
+        //          determine the missing element
+        // ------------------------------------------------------------
+        private static int FindTheMissingElement_VersionOne(int[] aintArrayOne, int[] aintArrayTwo)
+        {
+            int intMissingElement = 0;
+            int[] aintSmallArray = null;
+            int[] aintLargeArray = null;
+
+            // Sort the arrays
+            Array.Sort(aintArrayOne);
+            Array.Sort(aintArrayTwo);
+
+            // The array with the smaller length determines our loop
+            if (aintArrayOne.Length > aintArrayTwo.Length)
+            {
+                aintSmallArray = aintArrayTwo;
+                aintLargeArray = aintArrayOne;
+            }
+            else
+            {
+                aintSmallArray = aintArrayOne;
+                aintLargeArray = aintArrayTwo;
+            }
+
+            // Compare the elements in the sorted arrays
+            for (int intIndex = 0; intIndex < aintSmallArray.Length; intIndex++)
+            {
+                if (aintSmallArray[intIndex] != aintLargeArray[intIndex])
+                {
+                    intMissingElement = aintLargeArray[intIndex];
+                }
+            }
+
+            // If we didn't find any missing elements, it must be the last of 
+            // the large array
+            if (intMissingElement == 0) intMissingElement = aintLargeArray[aintLargeArray.Length - 1];
+
+            return intMissingElement;
+        }
+
+
+        // ------------------------------------------------------------
+        // Name: FindTheMissingElement_VersionTwo
+        // Purpose: Add array elements to a dictionary for the first (smaller)
+        //          array, and subtract from counts with the second (larger)
+        //          if we decrement a count to -1 we know it's our missing element
+        // ------------------------------------------------------------
+        private static int FindTheMissingElement_VersionTwo(int[] aintArrayOne, int[] aintArrayTwo)
+        {
+            int intMissingElement = 0;
+            Dictionary<int, int> dctElementCounts = new Dictionary<int, int>();
+            int intCurrentValue = 0;
+
+            // Assume aintArrayTwo is smaller for this exercise
+            foreach (int intElement in aintArrayTwo)
+            {
+                dctElementCounts.TryGetValue(intElement, out intCurrentValue);
+
+                dctElementCounts[intElement] = intCurrentValue + 1;
+            }
+
+            // Loop through second, larger array
+            foreach (int intElement in aintArrayOne)
+            {
+                dctElementCounts.TryGetValue(intElement, out intCurrentValue);
+
+                if (intCurrentValue == 0)
+                {
+                    intMissingElement = intElement;
+                }
+                else
+                {
+                    dctElementCounts[intElement] = intCurrentValue - 1;
+                }
+            }
+
+
+            return intMissingElement;
+        }
+
+    }
+}
